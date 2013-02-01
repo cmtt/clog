ac2a6c4d0990d7e61b5e66863456beee1b1b9ea9
cmtt <thoemmes+git@gmail.com>
2013-02-01 14:46:52 +0100

    configuration for text labels, added more colours and styles, added a support for logging date/time, global functions and a successful label.

--- test.js
+++ test.js
@@ -1,28 +1,46 @@
 var clog = require('./lib/clog');
 
 // display level configration:
-clog.configure({'log level': 2});
-//=> {'log': true, 'info': true, 'warn': false, 'error': false, 'debug': false}
+clog.configure({'log level': 3});
+//=> {'log': true, 'info': true, 'warn': true, 'error': false, 'success' : false, 'debug': false}
 
 // custom display configration:
 clog.configure({
   'log level': {
-    'log': true,
+    'log': false,
     'info': true,
-    'warn': false,
+    'warn': true,
     'error': true,
+    'success' : true,
     'debug': false
-  }
+  },
+  'date' : false, // requires the chrono module
+  'global' : false
 });
-//=> {'log': true, 'info': true, 'warn': false, 'error': true, 'debug': false}
+
+//=> {'log': false, 'info': true, 'warn': true, 'error': true, 'success' : true, 'debug': false}
 
 // clog usage:
 clog('server', 'start listening on port 3000');  // custom head
 
 clog.log('hello', 'world');                      // console.log
+clog.log('hello1', 'world2');                    // console.log
 clog.info(['foo', 'bar']);                       // console.info
 clog.warn('baz is deprecated.');                 // console.warn
 clog.error('HTTP/1.1 400 Bad Request');          // console.error
+
+clog.error(new Error('Falls apart at first touch'));
+clog.error(null,'Database updated');
+
 clog.debug('headers', {                          // console.debug
-    'Content-Type': 'text/javascript'
+  'Content-Type': 'text/javascript'
 });
+
+clog.configure({
+  'module' : true
+});
+
+clog.error('Database',null,'Images have been resized.');
+var err = new Error('Database is corrupted.');
+
+clog.error('Database',err);
